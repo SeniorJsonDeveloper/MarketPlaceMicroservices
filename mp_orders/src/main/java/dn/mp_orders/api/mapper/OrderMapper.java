84ed60156b0b98222ea76bc6990ca5bff31e552e
Daniil Novikov <146575982+SeniorJsonDeveloper@users.noreply.github.com>
2025-01-18 14:32:22 +0300

    Update OrderMapper.java

--- mp_orders/src/main/java/dn/mp_orders/api/mapper/OrderMapper.java
+++ mp_orders/src/main/java/dn/mp_orders/api/mapper/OrderMapper.java
@@ -8,5 +8,16 @@ import org.mapstruct.ReportingPolicy;
 @Mapper(componentModel = "spring",unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface OrderMapper extends Mappable<OrderEntity, OrderDto> {
 
+default public OrderDto mapToDto(OrderEntity order) {
+        OrderDto orderDto = new OrderDto();
+        orderDto.setId(order.getId());
+        orderDto.setName(order.getName());
+        orderDto.setMessage(order.getMessage());
+        orderDto.setStatus(order.getStatus());
+        orderDto.setPrice(order.getPrice());
+        orderDto.setWarehouseId(order.getWarehouseId());
+        var comments = commentRepository.findAllById(order.getCommentIds());
+        orderDto.setComments((List<CommentEntity>) comments);
+        return orderDto;
 
 }
